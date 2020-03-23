44b13c264b7fa9f4b2c48eb584ab5ebd8b45760b
Fallopio <kibaryshnikov@yandex.ru>
2020-03-23 19:45:17 +0400

    Updates

---
+++ precache-manifest.7a51d131aa80ca2ea22d6997976f3dfc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc9a2e65c8bdda4d0561608b961c6b00",
+    "url": "/CatsLandingRoute/index.html"
+  },
+  {
+    "revision": "40b3860c61e21c0aca3c",
+    "url": "/CatsLandingRoute/static/css/main.b42a4618.chunk.css"
+  },
+  {
+    "revision": "7525d188ee8aba860849",
+    "url": "/CatsLandingRoute/static/js/2.c4670215.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/CatsLandingRoute/static/js/2.c4670215.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40b3860c61e21c0aca3c",
+    "url": "/CatsLandingRoute/static/js/main.37a48985.chunk.js"
+  },
+  {
+    "revision": "faa8cca374c43bab64fc",
+    "url": "/CatsLandingRoute/static/js/runtime-main.322a03aa.js"
+  }
+]);
