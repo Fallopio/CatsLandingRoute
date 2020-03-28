5d518114903c0959c8e9a60c1abc38a641f44e69
Fallopio <kibaryshnikov@yandex.ru>
2020-03-29 03:22:10 +0400

    backgroud-fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -4,14 +4,19 @@ import './Footer.css'
 
 function Footer() {
     return (
-        <footer className>
-        <div className>
-            ipsum Lorem ipsum dolor sit amet
-        </div>
         <div>
-            ipsum Lorem ipsum dolor sit amet
+            <header className='footerhead'>
+                <h2>Some stuff</h2>
+            </header>
+            <footer className>
+                <div className>
+                    ipsum Lorem ipsum dolor sit amet
         </div>
-    </footer>
+                <div>
+                    ipsum Lorem ipsum dolor sit amet
+        </div>
+            </footer>
+        </div>
     )
 }
 
